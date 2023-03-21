2e3fc9e99befde83b1efb84598c1af866ca9415a
Vishal-Merugu <buntymerugu0@gmail.com>
2023-03-21 15:39:32 +0530

    seq.sync, .define , Sequelize s S , login

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host : "localhost",
-    user : "root",
-    database : "node-complete",
-    password : "Vishal@360"
+const sequelize = new Sequelize('node-complete', 'root', 'Vishal@360',{
+    dialect : 'mysql',
+    host : 'localhost'
 });
 
-module.exports = pool.promise()
+module.exports = sequelize;
